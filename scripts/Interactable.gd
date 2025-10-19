77db27e4a9dc18be2ea1e957791fe478a6b2a240
mwhittaker1 <mickeywhittaker@gmail.com>
2025-10-19 13:10:49 -0400

    working press e to interactprompot, needs cleanup

--- scripts/Interactable.gd
+++ scripts/Interactable.gd
@@ -4,5 +4,8 @@ extends Area2D
 signal interacted(by: Node)
 @export var prompt: String = "Interact"
 
-func interact(by: Node) -> void:
-	emit_signal("interacted", by)
+func interact(_by: Node = null) -> void:
+	# Locate the UI (the prompt is in group "PromptUI")
+	var prompt := get_tree().get_first_node_in_group("PromptUI")
+	if prompt:
+		prompt.show_prompt("Interact!", global_position, 1.5)
